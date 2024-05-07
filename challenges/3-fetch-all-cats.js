2e0754034bfe6f0159749fa3a8e72b92ce0d4fb8
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-05-07 12:10:42 +0100

    1-3 done

--- challenges/3-fetch-all-cats.js
+++ challenges/3-fetch-all-cats.js
@@ -1,6 +1,14 @@
 const fetchCatsByOwner = require("./1-fetch-cats-by-owner");
 const fetchAllOwners = require("./2-fetch-all-owners");
 
-const fetchAllCats = () => {};
+const fetchAllCats = () => {
+  return fetchAllOwners()
+    .then((owners) => {
+      return Promise.all(owners.map((owner) => fetchCatsByOwner(owner)));
+    })
+    .then((cats) => {
+      return cats.flat().sort();
+    });
+};
 
 module.exports = fetchAllCats;

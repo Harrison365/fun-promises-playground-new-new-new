67b15e2e900581345cd37644880aeb6c64e17e28
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-05-07 13:43:29 +0100

    dirname added to 4

--- challenges/4-find-pet.js
+++ challenges/4-find-pet.js
@@ -1,8 +1,9 @@
 const fs = require("fs/promises");
 
 const findPet = (name) => {
+  console.log(__dirname, "HEHEHEHEHE");
   return fs
-    .readFile(`./data/${name}.json`, "utf8")
+    .readFile(`${__dirname}/../data/${name}.json`, "utf8")
     .then((data) => {
       return JSON.parse(data);
     })

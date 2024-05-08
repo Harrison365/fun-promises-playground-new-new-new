5745b491888c76ec17561eb41eca038fe3269237
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-05-08 12:58:11 +0100

    extra challenge done :)

--- extra-challenges/promisification.js
+++ extra-challenges/promisification.js
@@ -1,14 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // do not alter this function
 function readSecretFile(cb) {
-  const filePath = path.join(__dirname, '../challenges/secret-message.txt');
-  fs.readFile(filePath, 'utf8', cb);
+  const filePath = path.join(__dirname, "../challenges/secret-message.txt");
+  fs.readFile(filePath, "utf8", cb);
 }
 
 function promisifiedReadSecretFile() {
-  // your code here
+  return new Promise((resolve, reject) => {
+    readSecretFile((err, content) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(content);
+    });
+  });
 }
 
 module.exports = { readSecretFile, promisifiedReadSecretFile };
